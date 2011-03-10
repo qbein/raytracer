2a4c80cddfd8c01422e63abf30726b2202ebc1f1
Kurt Aadnoy <kurt@aadnoy.info>
2011-03-10 21:22:59 +0100

    Initial Commit

---
+++ raytracer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  raytracer
+//
+//  Created by Kurt Aadnøy on 10.03.11.
+//  Copyright 2011 aadnoy.info. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
